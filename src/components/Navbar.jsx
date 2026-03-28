6d079ab0337ad40d1ba8c0d88387562093adc684
serigne fallou youm <youmserignefallou17@gmail.com>
2026-03-28 16:56:36 +0100

    responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,28 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import logo from "../assets/images/youm.jpg";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className="navbar">
-
+      
       <div className="logo">
         <img src={logo} alt="logo" />
         <h2>MonPortfolio</h2>
       </div>
 
-      <div className="nav-center">
-        <Link to="/">Accueil</Link>
-        <Link to="/apropos">À propos</Link>
-        <Link to="/projets">Projets</Link>
-        <Link to="/contact">Contact</Link>
+      {/* Bouton menu mobile */}
+      <div className="menu-icon" onClick={() => setOpen(!open)}>
+        ☰
+      </div>
+
+      <div className={`nav-center ${open ? "active" : ""}`}>
+        <Link to="/" onClick={() => setOpen(false)}>Accueil</Link>
+        <Link to="/apropos" onClick={() => setOpen(false)}>À propos</Link>
+        <Link to="/projets" onClick={() => setOpen(false)}>Projets</Link>
+        <Link to="/contact" onClick={() => setOpen(false)}>Contact</Link>
       </div>
 
       <div className="nav-right">
